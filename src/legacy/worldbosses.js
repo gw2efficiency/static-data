3984559d51a6151848b9540e3ca1f77143e5b98b
queicherius <queicherius@gmail.com>
2017-01-13 01:23:36 +0100

    Add legacy worldboss data

---
+++ src/legacy/worldbosses.js
@@ -0,0 +1,215 @@
+export default [
+  {
+    id: 1,
+    name: 'Tequatl the Sunless',
+    reward_dragonite: '15-25',
+    position: 'Sparkfly Fen',
+    times: ['02:00', '05:00', '09:00', '13:30', '18:00', '21:00'],
+    waypoint: '[&BNABAAA=]',
+    type: 'megaboss'
+  },
+  {
+    id: 2,
+    name: 'Inquest Golem Mark II',
+    reward_dragonite: '15-25',
+    position: 'Mount Maelstrom',
+    times: ['01:00', '04:00', '07:00', '10:00', '13:00', '16:00', '19:00', '22:00'],
+    waypoint: '[&BNQCAAA=]',
+    type: 'worldboss'
+  },
+  {
+    id: 3,
+    name: 'Fire Shaman',
+    reward_dragonite: '15-25',
+    position: 'Iron Marches',
+    waypoint: '[&BO4BAAA=]',
+    type: 'event'
+  },
+  {
+    id: 4,
+    name: 'Shadow Behemoth',
+    reward_dragonite: '3-5',
+    position: 'Queensdale',
+    times: ['01:45', '03:45', '05:45', '07:45', '09:45', '11:45', '13:45', '15:45', '17:45', '19:45', '21:45', '23:45'],
+    waypoint: '[&BPwAAAA=]',
+    type: 'worldboss'
+  },
+  {
+    id: 5,
+    name: 'Fire Elemental',
+    reward_dragonite: '3-5',
+    position: 'Metrica Province',
+    times: ['00:45', '02:45', '04:45', '06:45', '08:45', '10:45', '12:45', '14:45', '16:45', '18:45', '20:45', '22:45'],
+    waypoint: '[&BEcAAAA=]',
+    type: 'worldboss'
+  },
+  {
+    id: 6,
+    name: 'The Frozen Maw',
+    reward_dragonite: '3-5',
+    position: 'Wayfarer Foothills',
+    times: ['00:15', '02:15', '04:15', '06:15', '08:15', '10:15', '12:15', '14:15', '16:15', '18:15', '20:15', '22:15'],
+    waypoint: '[&BH4BAAA=]',
+    type: 'worldboss'
+  },
+  {
+    id: 7,
+    name: 'Jungle Wurm',
+    reward_dragonite: '3-5',
+    position: 'Caledon Forest',
+    times: ['01:15', '03:15', '05:15', '07:15', '09:15', '11:15', '13:15', '15:15', '17:15', '19:15', '21:15', '23:15'],
+    waypoint: '[&BEEFAAA=]',
+    type: 'worldboss'
+  },
+  {
+    id: 8,
+    name: 'The Shatterer',
+    reward_dragonite: '15-25',
+    position: 'Blazeridge Steppes',
+    times: ['00:00', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00', '21:00'],
+    waypoint: '[&BE4DAAA=]',
+    type: 'worldboss'
+  },
+  {
+    id: 9,
+    name: 'Claw of Jormag',
+    reward_dragonite: '15-25',
+    position: 'Frostgorge Sound',
+    times: ['01:30', '04:30', '07:30', '10:30', '13:30', '16:30', '19:30', '22:30'],
+    waypoint: '[&BHoCAAA=]',
+    type: 'worldboss'
+  },
+  {
+    id: 11,
+    name: 'Dredge Commissar',
+    reward_dragonite: '3-5',
+    position: 'Dredgehaunt Cliffs',
+    waypoint: '[&BD8FAAA=]',
+    type: 'event'
+  },
+  {
+    id: 12,
+    name: 'Eye of Zhaitan',
+    reward_dragonite: '3-5',
+    position: 'Straits of Devastation',
+    waypoint: '[&BKcCAAA=]',
+    type: 'event'
+  },
+  {
+    id: 13,
+    name: 'Foulbear Chieftain',
+    reward_dragonite: '0',
+    position: 'Fields of Ruin',
+    waypoint: '[&BE8BAAA=]',
+    type: 'event'
+  },
+  {
+    id: 14,
+    name: 'Ulgoth the Modniir',
+    reward_dragonite: '3-5',
+    position: 'Harathi Hinterlands',
+    times: ['00:30', '03:30', '06:30', '09:30', '12:30', '15:30', '18:30', '21:30'],
+    waypoint: '[&BLEAAAA=]',
+    type: 'worldboss'
+  },
+  {
+    id: 15,
+    name: 'Admiral Taidha Covington',
+    reward_dragonite: '3-5',
+    position: 'Bloodtide Coast',
+    times: ['02:00', '05:00', '08:00', '11:00', '14:00', '17:00', '20:00', '23:00'],
+    waypoint: '[&BKgBAAA=]',
+    type: 'worldboss'
+  },
+  {
+    id: 16,
+    name: 'Karka Queen',
+    reward_dragonite: '30',
+    position: 'Southsun Cove',
+    times: ['01:00', '04:00', '08:00', '12:30', '17:00', '20:00'],
+    waypoint: '[&BNcGAAA=]',
+    type: 'megaboss'
+  },
+  {
+    id: 17,
+    name: 'Rhendak the Crazed',
+    reward_dragonite: '0',
+    position: 'Diessa Plateau',
+    waypoint: '[&BNwAAAA=]',
+    type: 'event'
+  },
+  {
+    id: 18,
+    name: 'Gates of Arah',
+    reward_dragonite: '0',
+    position: 'Cursed Shore',
+    waypoint: '[&BCADAAA=]',
+    type: 'event'
+  },
+  {
+    id: 19,
+    name: 'Megadestroyer',
+    reward_dragonite: '0',
+    position: 'Mount Maelstrom',
+    times: ['02:30', '05:30', '08:30', '11:30', '14:30', '17:30', '20:30', '23:30'],
+    waypoint: '[&BM0CAAA=]',
+    type: 'worldboss'
+  },
+  {
+    id: 20,
+    name: 'Branded Devourer Queen',
+    reward_dragonite: '0',
+    position: 'Iron Marches',
+    waypoint: '[&BO8BAAA=]',
+    type: 'event'
+  },
+  {
+    id: 21,
+    name: 'Temple of Grenth',
+    reward_dragonite: '30',
+    position: 'Cursed Shore',
+    waypoint: '[&BCIDAAA=]',
+    type: 'event'
+  },
+  {
+    id: 22,
+    name: 'Temple of Melandru',
+    reward_dragonite: '30',
+    position: 'Cursed Shore',
+    waypoint: '[&BBsDAAA=]',
+    type: 'event'
+  },
+  {
+    id: 23,
+    name: 'Temple of Dwayna',
+    reward_dragonite: '30',
+    position: "Malchor's Leap",
+    waypoint: '[&BLACAAA=]',
+    type: 'event'
+  },
+  {
+    id: 24,
+    name: 'Temple of Lyssa',
+    reward_dragonite: '30',
+    position: "Malchor's Leap",
+    waypoint: '[&BK0CAAA=]',
+    type: 'event'
+  },
+  {
+    id: 25,
+    name: 'Temple of Balthazar',
+    reward_dragonite: '30',
+    position: 'Straits of Devastation',
+    waypoint: '[&BPoCAAA=]',
+    type: 'event'
+  },
+  {
+    id: 26,
+    name: 'Evolved Jungle Wurm',
+    reward_dragonite: '40',
+    position: 'Bloodtide Coast',
+    times: ['03:00', '06:00', '10:00', '14:30', '19:00', '22:00'],
+    waypoint: '[&BKoBAAA=]',
+    type: 'megaboss'
+  }
+]
