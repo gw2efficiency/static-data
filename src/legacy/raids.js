b90ffbb7dc1dd9fe8552ee5d9e6fe3645e39d785
queicherius <queicherius@gmail.com>
2017-03-02 05:35:00 +0100

    Add raid data

---
+++ src/legacy/raids.js
@@ -0,0 +1,17 @@
+export default [
+  {id: 'vale_guardian', raid: 'Forsaken Thicket', wing: 'Spirit Vale', name: 'Vale Guardian', type: 'Boss'},
+  {id: 'spirit_woods', raid: 'Forsaken Thicket', wing: 'Spirit Vale', name: 'Spirit Woods', type: 'Checkpoint'},
+  {id: 'gorseval', raid: 'Forsaken Thicket', wing: 'Spirit Vale', name: 'Gorseval', type: 'Boss'},
+  {id: 'sabetha', raid: 'Forsaken Thicket', wing: 'Spirit Vale', name: 'Sabetha', type: 'Boss'},
+  {id: 'slothasor', raid: 'Forsaken Thicket', wing: 'Salvation Pass', name: 'Slothasor', type: 'Boss'},
+  {id: 'bandit_trio', raid: 'Forsaken Thicket', wing: 'Salvation Pass', name: 'Bandit Trio', type: 'Boss'},
+  {id: 'matthias', raid: 'Forsaken Thicket', wing: 'Salvation Pass', name: 'Matthias', type: 'Boss'},
+  {id: 'escort', raid: 'Forsaken Thicket', wing: 'Stronghold of the Faithful', name: 'Escort', type: 'Boss'},
+  {id: 'keep_construct', raid: 'Forsaken Thicket', wing: 'Stronghold of the Faithful', name: 'Keep Construct', type: 'Boss'},
+  {id: 'twisted_castle', raid: 'Forsaken Thicket', wing: 'Stronghold of the Faithful', name: 'Twisted Castle', type: 'Checkpoint'},
+  {id: 'xera', raid: 'Forsaken Thicket', wing: 'Stronghold of the Faithful', name: 'Xera', type: 'Boss'},
+  {id: 'cairn', raid: 'Forsaken Thicket', wing: 'Bastion of the Penitent', name: 'Cairn', type: 'Boss'},
+  {id: 'mursaat_overseer', raid: 'Forsaken Thicket', wing: 'Bastion of the Penitent', name: 'Mursaat Overseer', type: 'Boss'},
+  {id: 'samarog', raid: 'Forsaken Thicket', wing: 'Bastion of the Penitent', name: 'Samarog', type: 'Boss'},
+  {id: 'deimos', raid: 'Forsaken Thicket', wing: 'Bastion of the Penitent', name: 'Deimos', type: 'Boss'}
+]
