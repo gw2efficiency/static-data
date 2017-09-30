76ca9a0f051138eea62ddbb882737c7d9d361c6d
queicherius <queicherius@gmail.com>
2017-10-01 00:05:25 +0100

    :card_index: Add new PoF statistics

--- generators/player-statistics.js
+++ generators/player-statistics.js
@@ -17,7 +17,7 @@ rows.map((row, i) => {
   ].join(', ')
 
   // 0           - 1     - 2           - 3     - 4   - 5       - 6     - 7           - 8
-  // group sort	- group	- title sort	- title	- key	- sprite	- gold	- by_playtime	- decimals
+  // group sort  - group - title sort  - title - key - sprite  - gold  - by_playtime - decimals
 
   console.log(`  {${json}}${comma}`)
 })
