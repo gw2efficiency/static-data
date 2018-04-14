6c1f892f5418e7839ae687f6c20bb64395dd93cb
queicherius <queicherius@gmail.com>
2018-04-14 23:40:31 +0100

    Add Trade Caches for Karma conversion (2)

--- generators/trade-caches.js
+++ generators/trade-caches.js
@@ -3,7 +3,7 @@ const input = ``
 const array = input.split('\n').map(x => x.split('\t'))
 
 const id = parseInt(array[0][0], 10)
-const karma = 245
+const karma = 630
 
 const resultIds = array[0].splice(1)
 const resultCount = array[2].splice(1)
