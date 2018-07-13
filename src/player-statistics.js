8214c95cf5cdf61a02642442fa19dc13231118c1
queicherius <queicherius@gmail.com>
2018-07-13 19:03:51 +0100

    Use the "buy" version of liquid gold to prevent abuse

--- src/player-statistics.js
+++ src/player-statistics.js
@@ -1,7 +1,7 @@
 export default [
   {group: 'Wealth', title: 'Account Value', key: 'values.summary.value', sprite: 'account-overview', gold: true, by_playtime: true, decimals: 0},
   {group: 'Wealth', title: 'Account Value (excl. Gemstore)', key: 'values.summary.valueMinusGemItems', sprite: 'account-overview', gold: true, by_playtime: true, decimals: 0},
-  {group: 'Wealth', title: 'Liquid Gold', key: 'values.summary.liquidSell', sprite: 'gold', gold: true, by_playtime: true, decimals: 0},
+  {group: 'Wealth', title: 'Liquid Gold', key: 'values.summary.liquidBuy', sprite: 'gold', gold: true, by_playtime: true, decimals: 0},
   {group: 'Wealth', title: 'Gold', key: 'statistics.gold', sprite: 'gold', gold: true, by_playtime: true, decimals: 0},
   {group: 'Wealth', title: 'Gems', key: 'statistics.gems', sprite: 'gem', gold: false, by_playtime: true, decimals: 0},
   {group: 'Wealth', title: 'Character Value', key: 'values.characters.value', sprite: 'account-characters', gold: true, by_playtime: true, decimals: 0},
