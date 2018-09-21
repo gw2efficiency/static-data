b2b890e97d6977f34ac02157fe6ff4a41820f8eb
queicherius <queicherius@gmail.com>
2018-09-21 15:50:18 +0100

    Update the list of raids

--- src/legacy/raids.js
+++ src/legacy/raids.js
@@ -3,19 +3,27 @@ export default [
   {id: 'spirit_woods', raid: 'Forsaken Thicket', wing: 'Spirit Vale', name: 'Spirit Woods', type: 'Checkpoint'},
   {id: 'gorseval', raid: 'Forsaken Thicket', wing: 'Spirit Vale', name: 'Gorseval', type: 'Boss'},
   {id: 'sabetha', raid: 'Forsaken Thicket', wing: 'Spirit Vale', name: 'Sabetha', type: 'Boss'},
+
   {id: 'slothasor', raid: 'Forsaken Thicket', wing: 'Salvation Pass', name: 'Slothasor', type: 'Boss'},
   {id: 'bandit_trio', raid: 'Forsaken Thicket', wing: 'Salvation Pass', name: 'Bandit Trio', type: 'Boss'},
   {id: 'matthias', raid: 'Forsaken Thicket', wing: 'Salvation Pass', name: 'Matthias', type: 'Boss'},
+
   {id: 'escort', raid: 'Forsaken Thicket', wing: 'Stronghold of the Faithful', name: 'Escort', type: 'Boss'},
   {id: 'keep_construct', raid: 'Forsaken Thicket', wing: 'Stronghold of the Faithful', name: 'Keep Construct', type: 'Boss'},
   {id: 'twisted_castle', raid: 'Forsaken Thicket', wing: 'Stronghold of the Faithful', name: 'Twisted Castle', type: 'Checkpoint'},
   {id: 'xera', raid: 'Forsaken Thicket', wing: 'Stronghold of the Faithful', name: 'Xera', type: 'Boss'},
+
   {id: 'cairn', raid: 'Bastion of the Penitent', wing: 'Bastion of the Penitent', name: 'Cairn', type: 'Boss'},
   {id: 'mursaat_overseer', raid: 'Bastion of the Penitent', wing: 'Bastion of the Penitent', name: 'Mursaat Overseer', type: 'Boss'},
   {id: 'samarog', raid: 'Bastion of the Penitent', wing: 'Bastion of the Penitent', name: 'Samarog', type: 'Boss'},
   {id: 'deimos', raid: 'Bastion of the Penitent', wing: 'Bastion of the Penitent', name: 'Deimos', type: 'Boss'},
+
   {id: 'soulless_horror', raid: 'Hall of Chains', wing: 'Hall of Chains', name: 'Soulless Horror', type: 'Boss'},
   {id: 'river_of_souls', raid: 'Hall of Chains', wing: 'Hall of Chains', name: 'River of Souls', type: 'Boss'},
   {id: 'statues_of_grenth', raid: 'Hall of Chains', wing: 'Hall of Chains', name: 'Statues of Grenth', type: 'Boss'},
-  {id: 'voice_in_the_void', raid: 'Hall of Chains', wing: 'Hall of Chains', name: 'Voice in the Void', type: 'Boss'}
+  {id: 'voice_in_the_void', raid: 'Hall of Chains', wing: 'Hall of Chains', name: 'Voice in the Void', type: 'Boss'},
+
+  {id: 'conjured_amalgamate', raid: 'Mythwright Gambit', wing: 'Mythwright Gambit', name: 'Conjured Amalgamate', type: 'Boss'},
+  {id: 'twin_largos', raid: 'Mythwright Gambit', wing: 'Mythwright Gambit', name: 'Twin Largos', type: 'Boss'},
+  {id: 'qadim', raid: 'Mythwright Gambit', wing: 'Mythwright Gambit', name: 'Qadim', type: 'Boss'}
 ]
