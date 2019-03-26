0259213c0a980b8136c6306e24fa55dd99f593fc
queicherius <queicherius@gmail.com>
2019-03-26 19:30:43 +0000

    Add Pharus to the list of legendary weapons

--- src/legendaries.js
+++ src/legendaries.js
@@ -35,6 +35,7 @@ export default [
   {id: 87109, name: 'Claw of the Khan-Ur', type: 'weapon', skin: 8051, precursorId: 87037, precursorName: 'Claw of Resolution', precursorSkin: 8046, generation: 2},
   {id: 87687, name: 'Verdarach', type: 'weapon', skin: 8145, precursorId: 87764, precursorName: 'Call of the Void', precursorSkin: 8177, generation: 2},
   {id: 88576, name: 'Xiuquatl', type: 'weapon', skin: 8389, precursorId: 88851, precursorName: 'Tlehco', precursorSkin: 8341, generation: 2},
+  {id: 89854, name: 'Pharus', type: 'weapon', skin: 8576, precursorId: 89886, precursorName: 'Spero', precursorSkin: 8600, generation: 2},
 
   {id: 81908, name: 'Aurora', type: 'trinket', skin: null, precursorId: 81729, precursorName: 'Spark of Sentience', precursorSkin: null, generation: -1},
 
