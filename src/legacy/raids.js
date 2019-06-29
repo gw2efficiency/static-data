474d521e7fde9a751c9238de1287f8bc0148845b
queicherius <queicherius@gmail.com>
2019-06-29 13:57:57 +0100

    Add data for new raid

--- src/legacy/raids.js
+++ src/legacy/raids.js
@@ -1,4 +1,5 @@
 export default [
+  // Forsaken Thicket
   {id: 'vale_guardian', raid: 'Forsaken Thicket', wing: 'Spirit Vale', name: 'Vale Guardian', type: 'Boss'},
   {id: 'spirit_woods', raid: 'Forsaken Thicket', wing: 'Spirit Vale', name: 'Spirit Woods', type: 'Checkpoint'},
   {id: 'gorseval', raid: 'Forsaken Thicket', wing: 'Spirit Vale', name: 'Gorseval', type: 'Boss'},
@@ -13,17 +14,26 @@ export default [
   {id: 'twisted_castle', raid: 'Forsaken Thicket', wing: 'Stronghold of the Faithful', name: 'Twisted Castle', type: 'Checkpoint'},
   {id: 'xera', raid: 'Forsaken Thicket', wing: 'Stronghold of the Faithful', name: 'Xera', type: 'Boss'},
 
+  // Bastion of the Penitent
   {id: 'cairn', raid: 'Bastion of the Penitent', wing: 'Bastion of the Penitent', name: 'Cairn', type: 'Boss'},
   {id: 'mursaat_overseer', raid: 'Bastion of the Penitent', wing: 'Bastion of the Penitent', name: 'Mursaat Overseer', type: 'Boss'},
   {id: 'samarog', raid: 'Bastion of the Penitent', wing: 'Bastion of the Penitent', name: 'Samarog', type: 'Boss'},
   {id: 'deimos', raid: 'Bastion of the Penitent', wing: 'Bastion of the Penitent', name: 'Deimos', type: 'Boss'},
 
+  // Hall of Chains
   {id: 'soulless_horror', raid: 'Hall of Chains', wing: 'Hall of Chains', name: 'Soulless Horror', type: 'Boss'},
   {id: 'river_of_souls', raid: 'Hall of Chains', wing: 'Hall of Chains', name: 'River of Souls', type: 'Boss'},
   {id: 'statues_of_grenth', raid: 'Hall of Chains', wing: 'Hall of Chains', name: 'Statues of Grenth', type: 'Boss'},
   {id: 'voice_in_the_void', raid: 'Hall of Chains', wing: 'Hall of Chains', name: 'Voice in the Void', type: 'Boss'},
 
+  // Mythwright Gambit
   {id: 'conjured_amalgamate', raid: 'Mythwright Gambit', wing: 'Mythwright Gambit', name: 'Conjured Amalgamate', type: 'Boss'},
   {id: 'twin_largos', raid: 'Mythwright Gambit', wing: 'Mythwright Gambit', name: 'Twin Largos', type: 'Boss'},
-  {id: 'qadim', raid: 'Mythwright Gambit', wing: 'Mythwright Gambit', name: 'Qadim', type: 'Boss'}
+  {id: 'qadim', raid: 'Mythwright Gambit', wing: 'Mythwright Gambit', name: 'Qadim', type: 'Boss'},
+
+  // The Key of Ahdashim
+  {id: 'gate', raid: 'The Key of Ahdashim', wing: 'The Key of Ahdashim', name: 'Gate', type: 'Checkpoint'},
+  {id: 'adina', raid: 'The Key of Ahdashim', wing: 'The Key of Ahdashim', name: 'Cardinal Adina', type: 'Boss'},
+  {id: 'sabir', raid: 'The Key of Ahdashim', wing: 'The Key of Ahdashim', name: 'Cardinal Sabir', type: 'Boss'},
+  {id: 'qadim_the_peerless', raid: 'The Key of Ahdashim', wing: 'The Key of Ahdashim', name: 'Qadim the Peerless', type: 'Boss'}
 ]
